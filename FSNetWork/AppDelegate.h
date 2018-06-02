4128341706036453c5cc4dc1d3edcd4175e0bfbc
523011150@qq.com <foreversun@zhichanghedemacbook-pro.local>
2018-06-02 23:15:56 +0800

    Initial Commit

---
+++ FSNetWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FSNetWork
+//
+//  Created by zhichang.he on 2018/6/2.
+//  Copyright © 2018年 zhichang.he. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FSNetWorkTests/FSNetWorkTests.m
@@ -0,0 +1,39 @@
+//
+//  FSNetWorkTests.m
+//  FSNetWorkTests
+//
+//  Created by zhichang.he on 2018/6/2.
+//  Copyright © 2018年 zhichang.he. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FSNetWorkTests : XCTestCase
+
+@end
+
+@implementation FSNetWorkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
